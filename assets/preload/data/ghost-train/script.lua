bfb562cd2032806b702b4295a4988a12dd8229e0
TheMigo9 <162376189+TheMigo9@users.noreply.github.com>
2024-03-12 16:29:33 +0200

    Add files via upload

---
+++ assets/preload/data/ghost-train/script.lua
@@ -0,0 +1,11 @@
+function opponentNoteHit()
+       health = getProperty('health')
+    if getProperty('health') > 0.1 then
+       setProperty('health', health- 0.02);
+	end
+
+    makeLuaText('watermark', "GHOST TRAIN MODERN|PORT BY MRINSANOXD", 0, -0,520 ); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark',20);
+    addLuaText('watermark');
+
+end
