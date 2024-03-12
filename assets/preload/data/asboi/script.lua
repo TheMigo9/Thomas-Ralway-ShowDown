3c00d26a2a54c91f754b6e0b7f7d24cbc8bdccf0
TheMigo9 <162376189+TheMigo9@users.noreply.github.com>
2024-03-12 16:22:58 +0200

    Add files via upload

---
+++ assets/preload/data/asboi/script.lua
@@ -0,0 +1,11 @@
+function opponentNoteHit()
+       health = getProperty('health')
+    if getProperty('health') > 0.1 then
+       setProperty('health', health- 0.02);
+	end
+
+    makeLuaText('watermark', "ASBOI|PORT BY MRINSANOXD", 0, -0,520 ); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark',20);
+    addLuaText('watermark');
+
+end
